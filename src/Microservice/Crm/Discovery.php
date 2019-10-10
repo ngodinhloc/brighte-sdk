4227fd8893d3885dec516cd2452c724f557bd675
Ken Ngo <ken.ngo@brighte.com.au>
2019-10-10 11:10:11 +1100

    remove sqs

--- src/Microservice/Crm/Discovery.php
+++ src/Microservice/Crm/Discovery.php
@@ -8,7 +8,4 @@ class Discovery
     public const API_ENDPOINT_PRODUCTION = 'https://api.brighte.com.au/v1/crm/';
     public const API_ENDPOINT_UAT = 'https://api.uat.brightelabs.com.au/v1/crm/';
     public const API_ENDPOINT_STAGING = 'https://api.staging.brightelabs.com.au/v1/crm/';
-
-    public const SQS_CONNECTION_CRM = 'sqs.ms.crm';
-    public const SQS_CONNECTION_PORTAL = 'sqs.portal';
 }
